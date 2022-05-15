fd2ea72bc0eac0e850db75c65059d7ddc296867d
someshkp <69900909+someshkp@users.noreply.github.com>
2022-05-15 22:36:50 +0530

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test="project-name">Video Stream App</h1>
+      <p data-ns-test="project-description">
+        I like to buit a video stream platform like netflix and amazon prime.
+      </p>
+    </div>
+  );
+};
 
 export default App;
